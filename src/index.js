c57e67f1191fd7b2f54fd3bd849d1610f173d095
Pangu <Pangu@DESKTOP-IF6DCCV>
2018-11-24 11:32:07 -0800

    react folder setup

--- src/index.js
+++ src/index.js
@@ -1,47 +1,7 @@
-import $ from 'jquery';
-import './style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-$('#login').click(() => {
-    console.log('button clicked');
-
-    import('./helpers').then(resp => {
-        console.log('Import Resp', resp);
-
-        const { login, addToDom } = resp;
-
-        
-        const result1 = login('SassySally', 'qwer');
-
-        const result2 = login('SomeRandom', 'gjh');
-
-        addToDom('h1', result1, 'green');
-        addToDom('h1', result2, 'red');
-    })
-});
-
-// $('#load-image').click(()=>{
-//     console.log('loading image');
-    
-//     import('./helpers').then(resp => {
-
-//         const { addImg } = resp;
-
-//         import('./webpackLogo.png').then(src => {
-//             addImg(src.default);
-//         });
-
-//     });
-// })
-
-$('#load-image').click( async()=>{
-    console.log('loading image');
-    
-    const resp = await import('./helpers');
-    
-    const { addImg } = resp;
-
-    const src = await import('./webpackLogo.png');
-
-    addImg(src.default);
-
-})
+ReactDOM.render(
+    <h1>Welcome to React</h1>,
+    document.getElementById('root')
+);
